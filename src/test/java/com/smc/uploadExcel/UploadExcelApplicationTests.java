8e71ae9cbbf2df9611fb8a2f7b2f30130cf199b3
Catherine <wangrt@cn.ibm.com>
2019-12-15 22:16:47 +0800

    test file modified

--- src/test/java/com/smc/uploadExcel/UploadExcelApplicationTests.java
+++ src/test/java/com/smc/uploadExcel/UploadExcelApplicationTests.java
@@ -1,13 +1,16 @@
 package com.smc.uploadExcel;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class UploadExcelApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
